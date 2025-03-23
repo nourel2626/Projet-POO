9900c5feb69ee55cf25778aa0d2e27874af1d00c
nourel2626 <nour.elmnouar@gmail.com>
2025-03-24 00:53:06 +0100

    Update ColliderNour1.cpp
    J'ai ajouté toutes mes méthodes et changé tous les "Centre" en "Position" même dans la fonction directionTo que tu viens d'ajouter

--- ColliderNour1.cpp
+++ ColliderNour1.cpp
@@ -9,62 +9,124 @@
 #include <algorithm>
 
 Collider::Collider(Collider const& autre)
-    : Centre(autre.Centre), Rayon(autre.Rayon)
+    : Position(autre.Position), Rayon(autre.Rayon)
 {}
 
+Collider::Collider(Vec2d position, double rayon){
+    if (rayon <0){
+        throw std::invalid_argument("Le rayon ne peut pas être négatif !!");
+    }
+    Rayon=rayon;
+    Position=position;
+}
 
+double Collider::distanceTo (Vec2d to){
+    return (directionTo(to)).length();
+}
 
 Vec2d Collider::getPosition() const&{
-    return Centre;
+    return Position;
 }
 double Collider::getRadius()const{
     return Rayon;
 }
 
+void Collider::move (Vec2d dx){
+    this -> Position = this -> Position + dx;
+}
+
+
 Collider& Collider::operator=(Collider& x){
     Rayon = x.getRadius();
-    Centre = x.getPosition();
+    Position = x.getPosition();
    return x;
 }
 
-Collider::Collider(Vec2d centre, double rayon){
-    if (rayon <0){
-        throw std::invalid_argument("Le rayon ne peut pas être négatif !!");
-    }
-    Rayon=rayon;
-    Centre=centre;
-};
-    void Collider::clamping(){
-        double worldSize = getAppConfig().simulation_world_size;
-        auto width  = worldSize; // largeur
-        auto height = worldSize; // hauteur
+bool Collider::operator>(Collider& body1) {
+   return body1.isPointInside(this->Position);
+}
 
-        double x,y;
-        while(Centre.operator[](0)<0 or Centre[1]<0){
-            if (Centre.operator[](0)<0){
-                x=Centre.operator[](0)+width; // On dit que la première coordonnée est selon l'axe x
-            }
-            if (Centre.operator[](0)>width){
-                x=Centre.operator[](0)-width;
-            }
-            if (Centre.operator[](1)<0){
-                y=Centre.operator[](1)+height; // On dit que la première coordonnée est selon l'axe x
-            }
-            if (Centre.operator[](1)>height){
-                y=Centre.operator[](1)-height;
-            }
+bool Collider::operator>(Vec2d p) {
+    return this->isPointInside(p);
+}
+Vec2d Collider::directionTo(Vec2d to){
+    double worldSize = getAppConfig().simulation_world_size;
+    auto width  = worldSize; // largeur
+    auto height = worldSize; // hauteur
 
-        }
-        Vec2d newCentre(x,y);
-        Centre=newCentre;
-
-
+    std::vector <Vec2d> candidats = {
+        to,
+        to + Vec2d(0, height),
+        to + Vec2d(0, -height),
+        to + Vec2d(width, 0),
+        to + Vec2d(-width, 0),
+        to + Vec2d(width, height),
+        to + Vec2d(width, -height),
+        to + Vec2d(-width, -height),
+        to + Vec2d(-width, height),
     };
-    Vec2d directionTo(Collider to){
+    Vec2d bestDirection = candidats[0] - Position;
+    double bestDistance = bestDirection.length();
+    for (const auto& i : candidats) {
+            Vec2d direction = i - Position;
+            double distance = direction.length();
+
+            if (distance < bestDistance) {
+                bestDirection = direction;
+                bestDistance = distance;
+            }
+        }
+
+        return bestDirection;
+    }
+
+
+bool Collider::isColliderInside(Collider other) {
+    return other.getRadius() <= this->Rayon && distanceTo(other.getPosition()) <= (this->Rayon - other.getRadius());
+}
+
+
+
+bool Collider::isPointInside(Vec2d p){
+ if (distanceTo(directionTo(p)) <= (this -> Rayon)){
+        return true;
+} else {
+        return false;
+}
+}
+void Collider::clamping(){
+      double worldSize = getAppConfig().simulation_world_size;
+      auto width  = worldSize; // largeur
+      auto height = worldSize; // hauteur
+
+      double x,y;
+      while(Position.operator[](0)<0 or Position[1]<0){
+          if (Position.operator[](0)<0){
+              x=Position.operator[](0)+width; // On dit que la première coordonnée est selon l'axe x
+          }
+          if (Position.operator[](0)>width){
+              x=Position.operator[](0)-width;
+          }
+          if (Position.operator[](1)<0){
+              y=Position.operator[](1)+height; // On dit que la première coordonnée est selon l'axe x
+          }
+          if (Position.operator[](1)>height){
+              y=Position.operator[](1)-height;
+          }
+
+}
+      Vec2d newPosition(x,y);
+      Position=newPosition;
+
+Vec2d directionTo(Collider to){
         double x(to.operator[](0),y(to.operator[](1));
-        x=Center.operator[](0)-x;
-        y=Center.operator[](1)-y;
+        x=Position.operator[](0)-x;
+        y=Position.operator[](1)-y;
         Vec2d retour(x,y);
         return retour;
     }
 
+
+};
+
+    
