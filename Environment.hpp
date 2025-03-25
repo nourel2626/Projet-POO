21b3b150890d690b29d10b1f4dad181421402ac3
nourel2626 <nour.elmnouar@gmail.com>
2025-03-25 16:20:22 +0100

    Update Environment.hpp

--- Environment.hpp
+++ Environment.hpp
@@ -8,14 +8,37 @@ class Animal;
 
 class Environment {
 public :
+      /*!
+     * @brief Ajoute un animal
+     * @param animal l'animal à ajouter à la liste d'animaux
+     */
     void addAnimal(Animal*& animal);
+    /*!
+     * @brief Ajoute une cible dans l'environnement
+     * @param cible la cible à ajouter à la liste de cibles
+     */
     void addTarget (const Vec2d& cible);
+    /*!
+     * @brief Permet de faire évoluer les animaux au cours du temps
+     * @param dt // à modifer plus tard
+     */
     void update(sf::Time dt);
+    /*!
+     * @brief Matérialise graphiquement les cibles sous forme de petits cercles rouges
+     * @param targetWindow la fenêtre sur laquelle va s'afficher le dessin
+     */
     void draw(sf::RenderTarget& targetWindow) const;
+    /*!
+     * @brief Permet de vider l'environnement de ses animaux et ses cibles
+     */
     void clean ();
-    Environment(const Environment&) = delete;
+
+    Environment(const Environment&) = delete; // Permet d'éviter la copie d'un Environment à partir d'un constructeur de copie
+
     ~Environment();
-    Environment& operator=(const Environment&) = delete;
+
+    Environment& operator=(const Environment&) = delete; // Permet d'éviter la copie d'un Environment par affectation
+
     Environment();
 private :
     std::list <Animal*> Animaux;
