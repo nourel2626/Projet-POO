9307106b7127829d186050d3c2c94154b5ca5210
nourel2626 <nour.elmnouar@gmail.com>
2025-03-31 17:52:59 +0200

    Update ColliderNour1.hpp

--- ColliderNour1.hpp
+++ ColliderNour1.hpp
@@ -1,6 +1,3 @@
-/*
- * POOSV 2025
- */
 
 #pragma once
 
@@ -31,13 +28,13 @@ class Collider
         Collider& operator=(Collider& x);
         Vec2d directionTo(Vec2d to);
         double distanceTo(Vec2d to);
-        
+        void move(const Vec2d& dx);
         bool isColliderInside (Collider other);
         bool isPointInside (Vec2d p);
         bool operator>(Collider& body1);
         bool operator>(Vec2d p);
         Vec2d directionTo(Collider to);
-        void move(const Vec2d& dx);
+        //void move(Vec2d& dx);
         bool isColliding(Collider other);
         bool operator|(Collider& body1);
         void operator+=(Vec2d dx);
