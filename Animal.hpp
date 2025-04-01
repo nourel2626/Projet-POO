95ac725d24102d85ab4bc177cd3c591d60b50e60
nourel2626 <nour.elmnouar@gmail.com>
2025-04-01 21:46:04 +0200

    Create Animal.hpp

---
+++ Animal.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <Utility/Vec2d.hpp>
+#include <Obstacle/Collider.hpp>
+
+enum Deceleration{forte, moyenne, faible};
+class Animal :public Collider{
+
+
+    public:
+        Animal();
+        Animal(const Vec2d& position);
+        double getStandardMaxSpeed();
+        double getMass();
+        void setTargetPosition(Vec2d Position);
+        Vec2d getSpeedVector();
+        void update(sf::Time dt);
+        void draw(sf::RenderTarget& targetWindow);
+        double getViewRange ();
+        double getViewDistance();
+        double getRotation();
+        void setRotation (double angle);
+        void drawVision(sf::RenderTarget& targetWindow);
+    private:
+        double Angle;
+        double DistanceVision;
+        Vec2d Direction;
+        double MagnitudeVitesse;
+        Vec2d PositionCible;
+       Vec2d ForceAttraction(Deceleration deceleration);
+    };
+
+
+
+
+
