a6549cd324aab721a43a771bc415a6352643bcc6
nourel2626 <nour.elmnouar@gmail.com>
2025-04-07 15:57:23 +0200

    Update Animal.hpp

--- Animal.hpp
+++ Animal.hpp
@@ -2,6 +2,9 @@
 #include <SFML/Graphics.hpp>
 #include <Utility/Vec2d.hpp>
 #include <Obstacle/Collider.hpp>
+#include "Random/Random.hpp"
+#include <list>
+#include <Environment/Environment.hpp>
 
 enum Deceleration{forte, moyenne, faible};
 class Animal :public Collider{
@@ -21,6 +24,11 @@ class Animal :public Collider{
         double getRotation();
         void setRotation (double angle);
         void drawVision(sf::RenderTarget& targetWindow);
+        double getRandomWalkRadius() const;
+        double getRandomWalkDistance () const;
+        double getRandomWalkJitter() const;
+        Vec2d randomWalk(Vec2d current_target);
+        Vec2d convertToGlobalCoord(const Vec2d& local) const;
         bool isTargetInSight(Vec2d positionCible);
     private:
         double Angle;
@@ -29,6 +37,7 @@ class Animal :public Collider{
         double MagnitudeVitesse;
         Vec2d PositionCible;
        Vec2d ForceAttraction(Deceleration deceleration);
+       Vec2d current_target = Vec2d(1, 0);
     };
 
 
