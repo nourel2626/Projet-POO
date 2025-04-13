0f1fe3fda7592ba3d13d174f870e80ecd88ea8b6
pdbellon <pdbellon@icloud.com>
2025-04-13 19:45:39 +0200

    Create Cactus.hpp

---
+++ Cactus.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <list>
+#include <Utility/Vec2d.hpp>
+#include <Utility/Utility.hpp>
+
+class Cactus :public OrganicEntity{
+public:
+    Cactus(Vec2d position);
+    ~Cactus();
+    void update(sf::Time dt);
+    void draw(sf::RenderTarget& targetWindow);
+private:
+    double Taille;
+
+};
