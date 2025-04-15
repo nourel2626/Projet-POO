fbeea3b89d4b424f34eec5143737aa75a266760c
pdbellon <pdbellon@icloud.com>
2025-04-15 18:32:56 +0200

    Update Cactus.cpp

--- Cactus.cpp
+++ Cactus.cpp
@@ -3,17 +3,34 @@
 #include <Environment/Cactus.hpp>
 
 Cactus::Cactus(Vec2d position)
-    :OrganicEntity(position, getAppConfig().food_energy)
+    :OrganicEntity(position, getAppConfig().food_energy,getAppConfig().food_energy)
 {
-    Taille = getAppConfig().food_energy;
+
 }
 
 Cactus::~Cactus(){
 }
 
-void Cactus::draw(sf::RenderTarget &targetWindow){
+void Cactus::draw(sf::RenderTarget &targetWindow) const{
     sf::Texture& texture = getAppTexture(getAppConfig().food_texture);
-    auto image_to_draw(buildSprite(getPosition(), Taille,texture));
-    targetWindow.draw(image_to_draw)
+    auto image_to_draw(buildSprite(getPosition(),getTaille(),texture));
+    targetWindow.draw(image_to_draw);
 
+}
+
+void Cactus::update(sf::Time  dt){
+
+}
+
+bool Cactus::eatable( OrganicEntity const* entity) const{
+    return entity->eatableBy(this);
+}
+bool Cactus::eatableBy(Scorpion const* scorpion) const{
+    return false;
+}
+bool Cactus::eatableBy(Lizard const* lizard) const{
+    return false;
+}
+bool Cactus::eatableBy(Cactus const* cactus) const{
+    return false;
 }
