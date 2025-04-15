01712ac96c6cc648322d333458da91b1bb979e5d
pdbellon <pdbellon@icloud.com>
2025-04-15 18:32:33 +0200

    Update Scorpion.hpp

--- Scorpion.hpp
+++ Scorpion.hpp
@@ -1,4 +1,4 @@
-#ifndef SCORPION_HPP
+#pragma once
 #define SCORPION_HPP
 
 #include <Animal/Animal.hpp>
@@ -6,18 +6,23 @@
 
 class Scorpion : public Animal{
 public:
-    Scorpion(Vec2d position, double energie, bool femelle);
+    Scorpion(Vec2d position,double taille, double energie, bool femelle);
     Scorpion(Vec2d position);
-    double getStandardMaxSpeed();
-    double getMass();
+    double getStandardMaxSpeed() const;
+    double getMass()const;
     double getRandomWalkRadius() const;
-    double getRandomWalkDistance() const;
-    double getRandomWalkJitter() const;
-    double getViewRange();
-    virtual sf::Texture& getTexture();
+    double getRandomWalkDistance()const;
+    double getRandomWalkJitter()const;
+    double getViewRange()const;
+    sf::Texture& getTexture()const;
+    void update(sf::Time dt);
+    void draw(sf::RenderTarget& targetWindow)const;
+    bool isTargetInSight(Vec2d positionCible);
+    bool eatableBy(Scorpion const* scorpion) const;
+    bool eatableBy(Lizard const* lizard) const;
+    bool eatableBy(Cactus const* cactus) const;
+    bool eatable(OrganicEntity const* entity) const;
+    void drawVision(sf::RenderTarget &targetWindow)const;
 private:
-    double Energie;
-    double Taille;
-    bool Femelle;
+
 };
-#endif // SCORPION_HPP
