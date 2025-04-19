38be08d51519aa91668c589f6426cefdc824c5bb
pdbellon <pdbellon@icloud.com>
2025-04-19 14:45:25 +0200

    Update Scorpion.cpp

--- Scorpion.cpp
+++ Scorpion.cpp
@@ -18,6 +18,9 @@ Scorpion::Scorpion(Vec2d position)
 double Scorpion::getStandardMaxSpeed()const{
     return getAppConfig().scorpion_max_speed;
 }
+double Scorpion::getMaxSpeed() const{
+    return Animal::getMaxSpeed();
+}
 double Scorpion::getMass()const{
     return getAppConfig().scorpion_mass;
 }
@@ -38,81 +41,26 @@ sf::Texture& Scorpion::getTexture()const{
     return getAppTexture(getAppConfig().scorpion_texture);
 }
 void Scorpion::update(sf::Time dt){
-    auto& env = getAppEnv();
-    std::list<Vec2d> ciblesPotentielles = env.getEntitiesInSightForAnimal(this);
-    Vec2d f;
-
-    if (ciblesPotentielles.empty()) {
-        // Aucun cible visible -> random walk
-        f = randomWalk();
-    } else {
-        // Choisir une cible visible
-        setTargetPosition(ciblesPotentielles.front());
-        f = ForceAttraction(Deceleration::moyenne);
-    }
-
-    Vec2d acceleration = f / getMass();
-    Vec2d nouvelle_vitesse = getSpeedVector() + acceleration * dt.asSeconds();
-    Vec2d nouvelle_direction = nouvelle_vitesse.normalised();
-
-    if (nouvelle_vitesse.length() > getStandardMaxSpeed()) {
-        nouvelle_vitesse = nouvelle_direction * getStandardMaxSpeed();
-    }
-
-    if (!isEqual(nouvelle_direction.lengthSquared(), 0.0)) {
-        setDirection(nouvelle_direction);
-    }
-
-
-    setMagnitudeVitesse(nouvelle_vitesse.length());
-
-    Vec2d old_position = getPosition();
-    Vec2d nouvelle_position = old_position + nouvelle_vitesse * dt.asSeconds();
-    Vec2d dx = nouvelle_position - old_position;
-
-    move(dx);
+    Animal::update(dt);
 }
 void Scorpion::draw(sf::RenderTarget& targetWindow)const{
     Animal::draw(targetWindow);
-//    sf::Texture& texture = getTexture();
-//    auto image_to_draw(buildSprite( getPosition(), getRadius()*2,texture,getRotation()*(1/DEG_TO_RAD)));
-//    targetWindow.draw(image_to_draw);
-//    //targetWindow.draw(buildCircle(PositionCible,5,sf::Color(255,0,0)));
-//     drawVision(targetWindow); // Problème ici
-//     sf::Color color(0, 0, 255);
-
-//     // Un anneau autour de la current_target
-
-//     auto& env = getAppEnv();
-//     std::list<Vec2d> ciblesPotentielles = env.getEntitiesInSightForAnimal(this);
-//     if (ciblesPotentielles.empty()){
-//     targetWindow.draw(buildAnnulus(getPosition() + getRandomWalkDistance()*getDirection(), getRandomWalkRadius(), sf::Color(255, 150, 0),2));
-//     targetWindow.draw(buildCircle(getPosition() + randomWalk(), 5, color));
-//}
 }
 void Scorpion::drawVision(sf::RenderTarget &targetWindow)const{
     Animal::drawVision(targetWindow);
-//    sf::Color color = sf::Color::Black;
-//    color.a = 16;  // Gris transparent
-
-//    double startAngle = (-getViewRange()*180/PI)/2.0; // Début de l'arc
-//    double endAngle = (getViewRange()*180/PI)/2.0;   // Fin de l'arc
-//    double rotation = getDirection().angle()/DEG_TO_RAD;  // Rotation pour aligner l'arc avec la direction
-
-//    Arc arc = buildArc(startAngle, endAngle, getViewDistance(), getPosition(), color, rotation);
-//    targetWindow.draw(arc);
 }
 bool Scorpion::isTargetInSight(Vec2d positionCible){
-    bool retour(false);
-    Vec2d d(positionCible-getPosition());
-    Vec2d dn(d.normalised());
-    double a(getDirection().dot(dn));
-    if ((d.lengthSquared()<= getViewDistance()*getViewDistance())and(a >= cos((getViewRange()+0.001)/2))){
-        retour=true;
-    }
-    if (isEqual(d.lengthSquared(),0)){
-        retour=true;
-    }
+      bool retour(Animal::isTargetInSight(positionCible));
+//    bool retour(false);
+//    Vec2d d(positionCible-getPosition());
+//    Vec2d dn(d.normalised());
+//    double a(getDirection().dot(dn));
+//    if ((d.lengthSquared()<= getViewDistance()*getViewDistance())and(a >= cos((getViewRange()+0.001)/2))){
+//        retour=true;
+//    }
+//    if (isEqual(d.lengthSquared(),0)){
+//        retour=true;
+//    }
     return retour;
 
 }
@@ -120,7 +68,7 @@ bool Scorpion::eatableBy(Scorpion const* scorpion) const{
     return false;
 }
 bool Scorpion::eatableBy(Lizard const* lizard) const{
-    return true;
+    return false;
 }
 bool Scorpion::eatableBy(Cactus const* cactus) const{
     return false;
