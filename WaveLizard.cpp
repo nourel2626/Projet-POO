c0c6694009cc7033b8ffc6ab9d96d067c5062b6a
pdbellon <pdbellon@icloud.com>
2025-05-25 21:47:32 +0200

    Update WaveLizard.cpp

--- WaveLizard.cpp
+++ WaveLizard.cpp
@@ -1,15 +1,23 @@
 #include <Animal/NeuronalScorpion/WaveLizard.hpp>
 #include <Environment/Wave.hpp>
-/*
-Wavelizard::WaveLizard(Vec2d position, double energie, bool femelle,double taille)
-    : Lizard(position, getAppConfig().lizard_longevity, femelle, taille)
+#include <Environment/Wave.hpp>
+#include <Application.hpp>
+
+WaveLizard::WaveLizard(Vec2d position, double energie, bool femelle,double taille)
+    : Lizard(position, energie, femelle, taille)
 {}
 
 WaveLizard::WaveLizard(Vec2d position)
     : Lizard(position)
 {}
 
-void WaveLizard::Emission() {
-    new Wave wave
+void WaveLizard::emission() {
+    Wave* wave = new Wave(getPosition(), getAppConfig().wave_default_energy, getAppConfig().wave_default_radius,
+                         getAppConfig().wave_default_mu, getAppConfig().wave_default_speed);
+    getAppEnv().addWave(wave);
 }
-*/
+void WaveLizard::update(sf::Time dt){
+    Lizard::update(dt);
+    emission();
+}
+
