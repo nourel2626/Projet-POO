b9663572a8a6f606742c12f0e49c68381e1e5cfa
nourel2626 <nour.elmnouar@gmail.com>
2025-05-25 21:19:03 +0200

    Create WaveLizard.hpp

---
+++ WaveLizard.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <Animal/Lizard.hpp>
+/*
+class Wave;
+class WaveLizard: public Lizard {
+public:
+    Wavelizard(Vec2d position, double energie, bool femelle,double taille);
+    WaveLizard(Vec2d position);
+    void Emission();
+private:
+    
+    
+};
+*/
