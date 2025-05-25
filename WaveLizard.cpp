60f737a147395ad62534db80b8b7917141ea331c
nourel2626 <nour.elmnouar@gmail.com>
2025-05-25 21:19:25 +0200

    Create WaveLizard.cpp

---
+++ WaveLizard.cpp
@@ -0,0 +1,15 @@
+#include <Animal/NeuronalScorpion/WaveLizard.hpp>
+#include <Environment/Wave.hpp>
+/*
+Wavelizard::WaveLizard(Vec2d position, double energie, bool femelle,double taille)
+    : Lizard(position, getAppConfig().lizard_longevity, femelle, taille)
+{}
+
+WaveLizard::WaveLizard(Vec2d position)
+    : Lizard(position)
+{}
+
+void WaveLizard::Emission() {
+    new Wave wave
+}
+*/
