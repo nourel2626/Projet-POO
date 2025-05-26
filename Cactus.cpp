2c12c20b5402fc2c781259db51c1d03392fd3157
nourel2626 <nour.elmnouar@gmail.com>
2025-05-26 11:20:12 +0200

    Update Cactus.cpp

--- Cactus.cpp
+++ Cactus.cpp
@@ -3,14 +3,8 @@
 #include <Environment/Cactus.hpp>
 
 Cactus::Cactus(Vec2d position)
-    : OrganicEntity(position, getAppConfig().food_energy, sf::seconds(1E9), getAppConfig().food_size)
+    : OrganicEntity(position, getAppConfig().food_energy, sf::seconds(1E9), getAppConfig().food_size) //Construction d'un cactus avec une longétivité "infinie", il ne disparaît que s'il est mangé
 {}
-
-
-
-Cactus::~Cactus(){
-}
-
 void Cactus::draw(sf::RenderTarget &targetWindow) const{
     OrganicEntity::draw(targetWindow);
 
@@ -19,10 +13,16 @@ sf::Texture& Cactus::getTexture() const {
     return getAppTexture(getAppConfig().food_texture);
 }
 void Cactus::update(sf::Time  dt){
+    auto& env= getAppEnv();
+    if(env.getHumidity()){
+        if(!(getEnergie()> getAppConfig().food_max_energy)){
+            setRadius(getRadius()+getAppConfig().food_growth_rate/2);
+            setEnergie(getEnergie()+getAppConfig().food_growth_rate);
+        }
+    }
 
 }
-
-bool Cactus::eatable( OrganicEntity const* entity) const{
+bool Cactus::eatable( OrganicEntity const* entity) const{ // double dispatch permettant de gérer la prédation les différents cas en fonction du type de l'entité
     return entity->eatableBy(this);
 }
 bool Cactus::eatableBy(Scorpion const* scorpion) const{
@@ -34,30 +34,31 @@ bool Cactus::eatableBy(Lizard const* lizard) const{
 bool Cactus::eatableBy(Cactus const* cactus) const{
     return false;
 }
-bool Cactus::matable(OrganicEntity const* other) const {
-     if (other->matable(this) && this->matable(other)) {
-         return true;
-     }
- }
-
- bool Cactus::canMate(Scorpion const* scorpion) const {
-     return false;
- }
- bool Cactus::canMate(Lizard const* lizard) const {
-     return false;
- }
- bool Cactus::canMate(Cactus const* food) const {
-     return false;
- }
- bool Cactus::meet (OrganicEntity *mate)  {
-      return mate->meet(this);
-  }
-  bool Cactus::meetWith (Scorpion *scorpion)  {
-      return false;
-  }
-  bool Cactus::meetWith (Lizard *lizard)  {
-      return false;
-  }
-  bool Cactus::meetWith (Cactus *cactus)  {
-      return false;
-  }
+bool Cactus::matable(OrganicEntity const* other) const { // double dispatch permettant de gérer la reproduction en fonction du type des entités
+    if (other->matable(this) && this->matable(other)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+bool Cactus::canMate(Scorpion const* scorpion) const {
+    return false;
+}
+bool Cactus::canMate(Lizard const* lizard) const {
+    return false;
+}
+bool Cactus::canMate(Cactus const* food) const {
+    return false;
+}
+bool Cactus::meet (OrganicEntity *mate)  { // double dispatch permettant de gérer la rencontre en fonction du type des entités
+    return mate->meet(this);
+}
+bool Cactus::meetWith (Scorpion *scorpion)  {
+    return false;
+}
+bool Cactus::meetWith (Lizard *lizard)  {
+    return false;
+}
+bool Cactus::meetWith (Cactus *cactus)  {
+    return false;
+}
