0528ce1e440e825545fd5f4f12ba69cc1777e315
nourel2626 <nour.elmnouar@gmail.com>
2025-05-26 11:33:29 +0200

    Update OrganicEntity.cpp

--- OrganicEntity.cpp
+++ OrganicEntity.cpp
@@ -9,12 +9,7 @@ OrganicEntity::OrganicEntity(Vec2d const& position, double energie, sf::Time lon
       Age(sf::Time::Zero),
       Longevite(longevite)
 {}
-
- double OrganicEntity::getTaille() const{
-     return getRadius();
- }
-
- OrganicEntity::~OrganicEntity()= default;
+OrganicEntity::~OrganicEntity()= default;
 
 double OrganicEntity::getEnergie() const {
     return Energie;
@@ -26,69 +21,68 @@ void OrganicEntity::draw(sf::RenderTarget& targetWindow) const{
     sf::Texture& texture = getTexture();
     auto image_to_draw(buildSprite( getPosition(), getRadius()*2,texture));
     targetWindow.draw(image_to_draw);
-     sf::Color color(0, 0, 255);
-     if (!isDebugOn()){
-         Collider::draw(targetWindow);
-         auto text2 = buildText(to_nice_string(OrganicEntity::getEnergie()), (getPosition()), getAppFont(),
+    sf::Color color(0, 0, 255);
+    if (!isDebugOn()){
+        draw(targetWindow);
+        auto text2 = buildText(to_nice_string(OrganicEntity::getEnergie()), (getPosition()), getAppFont(),
                                getAppConfig().default_debug_text_size, sf::Color::Black );
-         targetWindow.draw(text2);
-     }
+        targetWindow.draw(text2);
+    }
 
 }
 sf::Texture& OrganicEntity::getTexture() const{
     return getAppTexture(getAppConfig().entity_texture_tracked);
 }
 
-double OrganicEntity::getTaille(){
-     return getRadius();
- }
+double OrganicEntity::getTaille() const{
+    return getRadius();
+}
 bool OrganicEntity::isDead (){
-     bool retour = false;
-     if (Energie < getAppConfig().animal_min_energy) {
-         retour = true;
-     }
-     if(Age > Longevite){
-         retour=true;
-     }
+    bool retour = false;
+    if (Energie < getAppConfig().animal_min_energy) {
+        retour = true;
+    }
+    if(Age > Longevite){
+        retour=true;
+    }
 
-         return retour;
- }
-
- void OrganicEntity::updateAge(sf::Time dt){
-     Age += dt;
- }
+    return retour;
+}
+void OrganicEntity::updateAge(sf::Time dt){
+    Age += dt;
+}
 bool OrganicEntity::matable(OrganicEntity const* other) const {
-     return false;
- }
-  bool OrganicEntity::canMate(Scorpion const* scorpion) const {
-      return false;
-  }
-  bool OrganicEntity::canMate(Lizard const* lizard) const {
-      return false;
-  }
+    return false;
+}
+bool OrganicEntity::canMate(Scorpion const* scorpion) const {
+    return false;
+}
+bool OrganicEntity::canMate(Lizard const* lizard) const {
+    return false;
+}
 
-  bool OrganicEntity::canMate(Cactus const* food) const {
-      return false;
-  }
-  sf::Time OrganicEntity::getAge() const {
-      return Age;
-  }
-  void OrganicEntity::update(sf::Time dt) {
-      if (isDead()){
-          delete this;
+bool OrganicEntity::canMate(Cactus const* food) const {
+    return false;
+}
+sf::Time OrganicEntity::getAge() const {
+    return Age;
+}
+void OrganicEntity::update(sf::Time dt) {
+    if (isDead()){
+        delete this;
 
-      }
-      return updateAge(dt);
-  }
-  bool OrganicEntity::meet (OrganicEntity *mate) {
-       return false;
-   }
-   bool OrganicEntity::meetWith (Scorpion *scorpion) {
-       return false;
-   }
-   bool OrganicEntity::meetWith (Lizard *lizard) {
-       return false;
-   }
-   bool OrganicEntity::meetWith (Cactus *cactus){
-       return false;
-   }
+    }
+    return updateAge(dt);
+}
+bool OrganicEntity::meet (OrganicEntity *mate) {
+    return false;
+}
+bool OrganicEntity::meetWith (Scorpion *scorpion) {
+    return false;
+}
+bool OrganicEntity::meetWith (Lizard *lizard) {
+    return false;
+}
+bool OrganicEntity::meetWith (Cactus *cactus){
+    return false;
+}
