3121b0733e98ba2d99ac1cff9a9c866e6c30fab5
nourel2626 <nour.elmnouar@gmail.com>
2025-05-26 12:41:51 +0200

    Update CloudGenerator.hpp

--- CloudGenerator.hpp
+++ CloudGenerator.hpp
@@ -1,12 +1,36 @@
+#pragma once
+
 #include "Obstacle/Collider.hpp"
 #include "Random/Random.hpp"
 #include "Environment/Environment.hpp"
+#include <Utility/Vec2d.hpp>
 
-class CloudGenerator : public Collider{
+/**
+ * \brief Classe responsable de générer des nuages à intervalles réguliers.
+ *
+ * La classe CloudGenerator gère l’apparition de nouveaux nuages dans l’environnement
+ * en fonction du temps écoulé. Elle peut être mise à jour à chaque cycle de simulation.
+ */
+class CloudGenerator {
 public:
-    CloudGenerator(const Vec2d& position, double taille);
+    /**
+     * \brief Constructeur de la classe CloudGenerator.
+     * Initialise le générateur avec un temps écoulé à zéro.
+     */
+    CloudGenerator();
 
-    void update (sf::Time dt);
+    /**
+     * \brief Met à jour l'état du générateur de nuages.
+     * \param dt Temps écoulé depuis la dernière mise à jour.
+     * Si suffisamment de temps s’est écoulé, un nuage peut être généré.     */
+    void update(sf::Time dt);
+
+    /**
+     * \brief Génère un nouveau nuage.
+     * \return Un pointeur vers un objet Cloud nouvellement généré.
+     */
+    Cloud* Generator();
+
 private:
     sf::Time TimeSinceLastCloud;
 };
