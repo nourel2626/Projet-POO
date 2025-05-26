68df1749e0931e01e1c9563bb4ed8060e7402046
nourel2626 <nour.elmnouar@gmail.com>
2025-05-26 11:38:35 +0200

    Update ColliderNour1.hpp

--- ColliderNour1.hpp
+++ ColliderNour1.hpp
@@ -1,6 +1,3 @@
-/*
- * POOSV 2025
- */
 
 #pragma once
 
@@ -23,28 +20,132 @@
 class Collider
 {
     public:
+        /*!
+         * @brief Obtient la position actuelle du collider
+         *
+         * @return La position du centre du collider
+         */
         Vec2d getPosition() const&;
+        /*!
+         * @brief Obtient le rayon du collider
+         *
+         * @return Le rayon du collider
+         */
         double getRadius() const;
+        void setRadius(double rayon);
+        /*!
+         * @brief Constructeur principal
+         *
+         * @param centre La position du centre du collider
+         * @param rayon Le rayon du collider
+         */
         Collider(Vec2d centre, double rayon);
+        /*!
+         * @brief Force la position à rester dans les limites du monde (torique)
+         */
         void clamping();
+        /*!
+         * @brief Constructeur de copie
+         *
+         * @param autre Le collider à copier
+         */
         Collider(Collider const& autre);
+        /*!
+         * @brief Surcharge de l'opérateur d'affectation
+         *
+         * @param x Le collider à affecter
+         * @return Une référence vers l'objet courant
+         */
         Collider& operator=(Collider& x);
+        /*!
+         * @brief Calcule la direction normalisée vers un point donné
+         *
+         * @param to La destination
+         * @return Un vecteur unitaire dans la direction la plus courte vers `to`
+         */
         Vec2d directionTo(Vec2d to);
+        /*!
+         * @brief Calcule la distance minimale torique jusqu’à un point donné
+         *
+         * @param to Le point cible
+         * @return La distance minimale torique entre le centre et `to`
+         */
         double distanceTo(Vec2d to);
+        /*!
+         * @brief Déplace le collider selon un vecteur donné
+         *
+         * @param dx Le déplacement à appliquer
+         */
         void move(const Vec2d& dx);
-        bool isColliderInside (Collider other);
+        /*!
+         * @brief Teste si un autre collider est totalement contenu dans celui-ci
+         *
+         * @param other Le collider cible
+         * @return true si `other` est entièrement à l’intérieur de ce collider
+         */
+        bool isColliderInside (const Collider& other);
+        /*!
+         * @brief Teste si un point est à l’intérieur du collider
+         *
+         * @param p Le point à tester
+         * @return true si le point est à l’intérieur
+         */
         bool isPointInside (Vec2d p);
+        /*!
+         * @brief Surcharge de l’opérateur > (interprétation contextuelle)
+         */
         bool operator>(Collider& body1);
+        /*!
+         * @brief Surcharge de l’opérateur > pour les positions
+         */
         bool operator>(Vec2d p);
-        Vec2d directionTo(Collider to);
-        //void move(Vec2d& dx);
-        bool isColliding(Collider other);
+        /*!
+         * @brief Calcule la direction vers un autre collider
+         *
+         * @param to Le collider cible
+         * @return Vecteur unitaire de direction vers `to`
+         */
+        Vec2d directionTo(const Collider& to);
+        /*!
+         * @brief Teste la collision avec un autre collider
+         *
+         * @param other Le collider cible
+         * @return true en cas de chevauchement
+         */
+        bool isColliding(const Collider& other);
+        /*!
+         * @brief Surcharge de l'opérateur | (interprétation contextuelle, probablement test de collision)
+         */
         bool operator|(Collider& body1);
+        /*!
+         * @brief Surcharge de l'opérateur += pour déplacer le collider
+         *
+         * @param dx Le vecteur de déplacement
+         */
         void operator+=(Vec2d dx);
-        double distanceTo(Collider to);
-        virtual void draw(sf::RenderTarget& target) const = 0;
+        /*!
+         * @brief Calcule la distance torique vers un autre collider
+         *
+         * @param to Le collider cible
+         * @return Distance minimale torique entre les centres
+         */
+        double distanceTo(const Collider& to);
+        /*!
+         * @brief Méthode virtuelle pure pour afficher le collider
+         *
+         * @param target Cible de rendu SFML
+         */
+        virtual void draw(sf::RenderTarget& target) const;
+        virtual ~Collider() = default;
     private:
         Vec2d Position;
         double Rayon;
 };
-        std::ostream& operator<<(std::ostream& os,Collider body);
+        /*!
+         * @brief Surcharge de l’opérateur de flux pour afficher un collider
+         *
+         * @param os Le flux de sortie
+         * @param body Le collider à afficher
+         * @return Le flux modifié
+         */
+        std::ostream& operator<<(std::ostream& os,const Collider& body);
