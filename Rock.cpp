413ffa001a3b4d5aa417a3329f8b221fd5479db9
nourel2626 <nour.elmnouar@gmail.com>
2025-05-26 11:36:27 +0200

    Update Rock.cpp

--- Rock.cpp
+++ Rock.cpp
@@ -20,18 +20,10 @@ Rock::Rock(const Vec2d& position)
 
 void Rock::draw(sf::RenderTarget& targetWindow) const {
     sf::Sprite rockSprite;
-
-        // Charge la texture depuis AppTexture
         rockSprite.setTexture(getAppTexture(getAppConfig().rock_texture));
-
-        // Centre le sprite sur son origine
         rockSprite.setOrigin(rockSprite.getLocalBounds().width / 2, rockSprite.getLocalBounds().height / 2);
-
-        // Positionne et oriente le sprite
-        rockSprite.setPosition(getPosition());  // getPosition() hérité de Collider
-        rockSprite.setRotation(Orientation * 180.0 / PI);       // Conversion en degrés
-
-        // Mise à l’échelle (le sprite par défaut fait généralement 100x100 px)
+        rockSprite.setPosition(getPosition());  
+        rockSprite.setRotation(Orientation * 180.0 / PI);       
         double scale = 2.0 * getRadius() / rockSprite.getLocalBounds().width;
         rockSprite.setScale(scale, scale);
 
